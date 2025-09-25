5e0d2976055adc21660141c4ba84a18dc715ae0d
ESievierin <egor.sievierin@gmail.com>
2025-09-25 13:18:06 +0300

    CRPT-5 fixed converter naming

--- CryptoApp.Presentation/Converters/ArrayLenghtToVisibilityConverter.cs
+++ CryptoApp.Presentation/Converters/ArrayLenghtToVisibilityConverter.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace CryptoApp.Presentation.Converters
 {
-    public class BoolToVisibilityConverter : IValueConverter
+    public class ArrayLenghtToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
